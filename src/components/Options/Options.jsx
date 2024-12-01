7200bb232683e8f298411f97b7de84c21052b377
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2024-12-01 12:22:34 +0200

    fixed reset button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -10,12 +10,7 @@ export default function Options({
       <button onClick={() => updateFeedback("good")}>Good</button>
       <button onClick={() => updateFeedback("neutral")}>Neutral</button>
       <button onClick={() => updateFeedback("bad")}>Bad</button>
-      <button
-        display={totalFeedback ? "block" : "none"}
-        onClick={resetFeedback}
-      >
-        Reset
-      </button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
     </div>
   );
 }
